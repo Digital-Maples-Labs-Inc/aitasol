3d809631d5a4ff78b97331abc3b48b82b80a649f
takawiramundure <takawiramundure@gmail.com>
2026-01-02 18:45:16 -0500

    Fix Babel config for webpack and add placeholder assets
    - Conditionally exclude react-native-reanimated plugin for web builds
    - Create placeholder images (favicon, icon, splash, adaptive-icon)
    - Fixes webpack build errors and blank screen issue

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,22 @@
 module.exports = function(api) {
   api.cache(true);
+  
+  // Check if building for web (webpack)
+  const isWebpack = api.caller((caller) => {
+    return caller && (caller.name === 'babel-loader' || caller.platform === 'web');
+  });
+  
+  const plugins = ['nativewind/babel'];
+  
+  // react-native-reanimated plugin causes issues with webpack
+  // Only include it for native builds
+  if (!isWebpack) {
+    plugins.push('react-native-reanimated/plugin');
+  }
+  
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'nativewind/babel',
-      'react-native-reanimated/plugin',
-    ],
+    plugins: plugins,
   };
 };
 
