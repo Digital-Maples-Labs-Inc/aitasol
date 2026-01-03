0990aaec49c056b0f4650240d8f35001dea2b705
takawiramundure <takawiramundure@gmail.com>
2026-01-02 19:31:06 -0500

    Add webpack path alias configuration and install expo-status-bar
    - Create webpack.config.js to resolve @/ path aliases
    - Install expo-status-bar package
    - Configure webpack to match tsconfig.json paths

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/**
+ * Webpack configuration for Expo web builds
+ * Extends @expo/webpack-config to add path aliases
+ */
+
+const { createWebpackConfigAsync } = require('@expo/webpack-config');
+const path = require('path');
+
+module.exports = async function (env, argv) {
+  const config = await createWebpackConfigAsync(env, argv);
+
+  // Add path aliases to match tsconfig.json
+  config.resolve = config.resolve || {};
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@': path.resolve(__dirname, 'src'),
+    '@/components': path.resolve(__dirname, 'src/components'),
+    '@/screens': path.resolve(__dirname, 'src/screens'),
+    '@/services': path.resolve(__dirname, 'src/services'),
+    '@/types': path.resolve(__dirname, 'src/types'),
+    '@/utils': path.resolve(__dirname, 'src/utils'),
+    '@/hooks': path.resolve(__dirname, 'src/hooks'),
+    '@/contexts': path.resolve(__dirname, 'src/contexts'),
+  };
+
+  return config;
+};
+
