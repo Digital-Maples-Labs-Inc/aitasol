921fb0cf0012f2bc246b0d0e926d3038a9ed8f59
takawiramundure <takawiramundure@gmail.com>
2026-01-02 19:26:33 -0500

    Remove NativeWind from Babel config - not used and causing webpack errors

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,9 @@
 module.exports = function(api) {
   api.cache(true);
   
-  const plugins = ['nativewind/babel'];
-  
-  // react-native-reanimated plugin causes issues with webpack
-  // Since we're primarily targeting web, exclude it for now
-  // If you need it for native builds, you can conditionally add it
-  // For web builds, this plugin is not needed and causes errors
-  
+  // Minimal config - NativeWind not used in codebase, removed to fix webpack errors
   return {
     presets: ['babel-preset-expo'],
-    plugins,
   };
 };
 
